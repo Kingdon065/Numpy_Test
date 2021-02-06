da6ed8ed57371de67ba4105364f2142faad1fdcf
Kingdon065 <masiyuan90@163.com>
2021-02-06 19:19:07 +0800

    1.3

--- numpy06_rw_test.py
+++ numpy06_rw_test.py
@@ -6,7 +6,7 @@ print(i2)
 #np.savetxt('eye.txt', i2)
 
 c, v = np.loadtxt('data.csv', delimiter=',', usecols=(6, 7), unpack=True)
-print(c, v)
+
 vwap = np.average(c, weights=v)
 print(f'VWAP = {vwap}')
 
@@ -16,3 +16,10 @@ print(f'mean = {mean}')
 t = np.arange(len(c))
 twap = np.average(c, weights=t)
 print(f'TWAP = {twap}')
+
+h, l = np.loadtxt('data.csv', delimiter=',', usecols=(4 ,5), unpack=True)
+print(f'highest: {np.max(h)}')
+print(f'lowest: {np.min(l)}')
+
+print(f'Spread high price: {np.ptp(h)}')
+print(f'Spread low price: {np.ptp(l)}')
--- test.py
+++ test.py
@@ -1,6 +1,33 @@
-from tkinter import *
-from tkinter import font
+from random import random, randint
+import csv
 
-root = Tk()
+def get_price():
+    f = random()
+    i = randint(330, 365)
+    num = i + f
+    return f'{num:.2f}'
 
-print(font.families())
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    for i in range(20):
+        temp = [get_price() for i in range(4)]
+        max_price = max(temp)
+        temp.remove(max_price)
+        min_price = min(temp)
+        temp.remove(min_price)
+        
+        prices = []
+        index = randint(0, 1)
+        prices.append(temp[index])
+        prices.append(max_price)
+        prices.append(min_price)
+        if index:
+            prices.append(temp[0])
+        else:
+            prices.append(temp[1])
+        print(prices)
+        
+        row = ['AAPL', f'{8+i:02}-01-2011', ' '] + prices
+        row.append(randint(20000000, 25000000))
+        writer.writerow(row)
